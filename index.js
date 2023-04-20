89bec0d216b4e684df3a39aaf02386bc43a65059
jaswanthmeesala <jaswanthjassu111@gmail.com>
2023-04-20 20:00:55 +0530

    calculator

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ document.addEventListener("keypress",(event) => {
     {
         document.getElementById("out").innerHTML=eval(exp);
     }
+    else if(event.key==="Delete")
+    {
+        exp=exp.slice(0,exp.length-1);
+        document.getElementById("out").innerHTML=exp;
+    }
+    else if(event.key>=0 && event.key<=9)
+    {
+        exp=exp+String(event.key);
+        document.getElementById("out").innerHTML=exp;
+    }
+    else if(event.key==="+" || event.key==="-" || event.key==="*" || event.key==="/")
+    {
+        if(exp.charAt(exp.length-1)!="+" && exp.charAt(exp.length-1)!="-" && exp.charAt(exp.length-1)!="*" && exp.charAt(exp.length-1)!="/")
+        {
+            exp=exp+String(event.key);
+            document.getElementById("out").innerHTML=exp;
+        }
+    }
+    console.log(event.key);
 });
 
 
